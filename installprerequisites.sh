6930f6b982301a5f9a77a433cf0791fdd239e08e
eastoncrafter <74274138+eastoncrafter@users.noreply.github.com>
2022-04-12 21:07:00 -0400

    Delete installprerequisites.sh

--- installprerequisites.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if (( $EUID != 0 )); then
-    echo "Please run as root"
-    exit
-fi
-apt install libclang-dev build-essential -y
-apt install curl wget git -y
-apt install net-tools -y
-exit
