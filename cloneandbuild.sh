cb4d810537971c5d5e3578ba8914242a089abaf1
eastoncrafter <gamejumper49@gmail.com>
2022-04-12 23:16:19 -0400

    moved rust installer to cloneandbuild.sh

--- cloneandbuild.sh
+++ cloneandbuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd ~
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#Proceed with install using default options.  Script will add  $HOME/.cargo/env to your .profile, so ’source’ it after install finishes….
 source ~/.profile
 source $HOME/.cargo/env
 source ~/.bashrc
--- installprerequisites.sh
+++ installprerequisites.sh
@@ -6,7 +6,5 @@ fi
 apt install libclang-dev build-essential -y
 apt install curl wget git -y
 apt install net-tools -y
-cd ~
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-#Proceed with install using default options.  Script will add  $HOME/.cargo/env to your .profile, so ’source’ it after install finishes….
+
 exit
